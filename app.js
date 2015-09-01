5d2d90fce023d2d36a09607805f819f3d9224851
Kana Natsuno <dev@whileimautomaton.net>
2015-09-01 17:50:37 +0900

    Simplify manual dispatching with aiTable

--- app.js
+++ app.js
@@ -954,14 +954,7 @@ var shephy = {};
           AUTOMATED_MOVE_DELAY
         );
       } else {
-        if (player == 'human') {
-          setUpUIToChooseMove(gt, v);
-        } else {
-          setTimeout(
-            function () {processMove(gt.moves[random(gt.moves.length)]);},
-            AUTOMATED_MOVE_DELAY * 2
-          );
-        }
+        aiTable[player](gt, v);
       }
     }
   }
