557d23d6c5030358c5346c41271c652f2790654c
Kana Natsuno <dev@whileimautomaton.net>
2015-11-30 20:18:40 +0900

    Bias toward sheep ranks rather thank sheep count

--- app.js
+++ app.js
@@ -899,8 +899,7 @@ var shephy = {};
     // TODO: Improve scoring.  For example:
     // * Exile bad cards as soon as possible.
     // * Use bad cards as soon as possible if their effects are small enough.
-    // * Bias towarad sheep sheep ranks rather than number of sheep.
-    var ranks = gameTree.world.field.map(function (c) {return c.rank;});
+    var ranks = gameTree.world.field.map(function (c) {return c.rank * c.rank;});
     return ranks.reduce(function (ra, r) {return ra + r;}, 0);
   }
 
